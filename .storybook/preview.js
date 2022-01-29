5920acb7b8581e0f9287f9446852ddbe86b7c52a
Fernando Zabin <fernando.zabin@gmail.com>
2022-01-28 22:33:06 -0300

    Add removeBg prop to GlobalStyles to fix Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,11 @@
 import { ThemeProvider } from 'styled-components'
-
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyles removeBg />
       <Story />
     </ThemeProvider>
   )
